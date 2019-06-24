0dfbf1c044c6fca0bce64725237a687b37081bcd
cvs2910 <cvs2910@gmail.com>
2019-06-23 22:55:56 -0600

    Add files via upload

---
+++ GAPTestCase1/GAPTestCase1/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GAPTestCase1
+{
+    public class Methods
+    {
+        /// <summary>
+        /// Método que se encarga de mezclar cadenas de caracteres, ordenando los campos según su posición en la cadena origen.
+        /// </summary>
+        /// <param name="stringArray">Recibe un tipo List<string></param>
+        /// <returns>Retorna un cadena String, que contiene los datos mezclados en orden, de las cadenas de la lista.</returns>
+        public static string StringMerge(List<string> stringArray)
+        {
+            string resultString = "";
+            char[] array = null;
+            int iterator;
+
+            stringArray.ForEach(item => {
+                array = item.ToCharArray();
+                iterator = 0;
+                for (int i = 0; i < item.Length; i++)
+                {
+                    iterator++;
+                    if (resultString.Length >= (i+iterator))
+                    { 
+                        resultString = resultString.Insert((i+iterator), array.GetValue(i).ToString());
+                    }
+                    else
+                    {
+                        resultString += array.GetValue(i).ToString();
+                    }
+                }
+            }); 
+
+            return resultString;
+        }
+    }
+}
---
+++ GAPTestCase1/GAPTestCase1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GAPTestCase1
+{
+    public class Program
+    {
+        public Program()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            string result = "";
+            var firstString = "abc";
+            var secondString = "efg";
+
+
+            List<string> stringList = new List<string>();
+            stringList.Add(firstString);
+            stringList.Add(secondString);
+
+            result = Methods.StringMerge(stringList);
+            Console.WriteLine("Test Case 1 - Cadenas de string a mezclar:\n1- " + firstString + "\n2- " + secondString+ "\n");
+            Console.WriteLine("Resultado Test Case 1: "+result);
+
+            stringList.Clear();
+            secondString = "efg123";
+            stringList.Add(firstString);
+            stringList.Add(secondString);
+            result = Methods.StringMerge(stringList);
+
+            Console.WriteLine("\n\nTest Case 2 - Cadenas de string a mezclar:\n1- " + firstString + "\n2- " + secondString + "\n");
+            Console.WriteLine("Resultado Test Case 2: " + result);
+
+            stringList.Clear();
+            firstString = "abc123456";
+            secondString = "efg";
+            stringList.Add(firstString);
+            stringList.Add(secondString);
+            result = Methods.StringMerge(stringList);
+
+            Console.WriteLine("\n\nTest Case 3 - Cadenas de string a mezclar:\n1- " + firstString + "\n2- " + secondString + "\n");
+            Console.WriteLine("Resultado Test Case 3: " + result);
+
+            Console.WriteLine("\nOprimir cualquier tecla para terminar el programa...");
+            Console.ReadKey();                
+
+        }
+
+
+
+    }
+}
